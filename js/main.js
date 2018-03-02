cc9051cc45099dcb272460f92500aa8369e9a842
Andrey Sokolov <sokolov-as-i@yandex.ru>
2018-03-03 01:52:43 +0300

    codestyle updates

--- js/main.js
+++ js/main.js
@@ -1,26 +1,28 @@
-var SOCKET_HOST = require('electron').remote.getGlobal('SOCKET_HOST');
+const SOCKET_HOST = require("electron").remote.getGlobal("SOCKET_HOST");
 
-var timer_time = (new Date).getTime() / 1000 + 20,
+let timer_time = new Date().getTime() / 1000 + 20,
     is_timer_on = true;
 
 console.log(window.location);
 console.log(SOCKET_HOST);
 
-var tick_timer = function () {
+const tick_timer = function() {
     if (!is_timer_on) return;
 
-    var _secs_left = Math.round(timer_time - ((new Date).getTime() / 1000));
+    let _secs_left = Math.round(timer_time - new Date().getTime() / 1000);
     //console.log(_secs_left);
     if (String(_secs_left % 60).length == 1) {
-        document.getElementById("t_sec").innerHTML = "0" + String(_secs_left % 60);
+        document.getElementById("t_sec").innerHTML =
+            "0" + String(_secs_left % 60);
     } else {
         document.getElementById("t_sec").innerHTML = _secs_left % 60;
     }
 
-    var _mins_left = Math.round(_secs_left / 60);
+    let _mins_left = Math.round(_secs_left / 60);
     //console.log(_mins_left);
     if (String(_mins_left % 60).length == 1) {
-        document.getElementById("t_min").innerHTML = "0" + String(_mins_left % 60);
+        document.getElementById("t_min").innerHTML =
+            "0" + String(_mins_left % 60);
     } else {
         document.getElementById("t_min").innerHTML = _mins_left % 60;
     }
@@ -30,15 +32,17 @@ var tick_timer = function () {
 
 setInterval(tick_timer, 1000);
 
-
 // SOCKETS
 
-var socket = io(SOCKET_HOST);
+const socket = io(SOCKET_HOST);
 
 socket.on("set_timer", function(msg) {
-    var _time = Math.round((new Date).getTime() / 1000);
+    let _time = Math.round(new Date().getTime() / 1000);
     _time_day_start = _time - _time % (60 * 60 * 24) - 3 * 3600;
-    timer_time = _time_day_start + parseInt(msg.t_hour) * 60 * 60 + parseInt(msg.t_min) * 60;
+    timer_time =
+        _time_day_start +
+        parseInt(msg.t_hour) * 60 * 60 +
+        parseInt(msg.t_min) * 60;
     $("#t_event_name").text(msg.event_name);
     tick_timer();
     is_timer_on = true;
@@ -53,21 +57,26 @@ socket.on("set_presenter", function(msg) {
 socket.on("update_mode", function(msg) {
     if (msg == "_mode_timer") {
         $("#title_cnt,#logo_cnt").addClass("_smalled");
-        $("#timer_cnt,#title_cnt,#background_container,#tree_cnt,#logo_cnt").removeClass("_hidden");
+        $(
+            "#timer_cnt,#title_cnt,#background_container,#tree_cnt,#logo_cnt"
+        ).removeClass("_hidden");
         $("#presenter_cnt").addClass("_hidden");
-    }
-    else if (msg == "_mode_presenter") {
+    } else if (msg == "_mode_presenter") {
         $("#presenter_cnt").removeClass("_hidden");
         $("#title_cnt,#logo_cnt").removeClass("_smalled");
-        $("#timer_cnt,#tree_cnt,#background_container,#title_cnt,#logo_cnt").addClass("_hidden");
-    }
-    else if (msg == "_mode_hide_all") {
+        $(
+            "#timer_cnt,#tree_cnt,#background_container,#title_cnt,#logo_cnt"
+        ).addClass("_hidden");
+    } else if (msg == "_mode_hide_all") {
         $("#title_cnt,#logo_cnt").removeClass("_smalled");
-        $("#timer_cnt,#presenter_cnt,#title_cnt,#tree_cnt,#logo_cnt,#background_container").addClass("_hidden");
-    }
-    else {
+        $(
+            "#timer_cnt,#presenter_cnt,#title_cnt,#tree_cnt,#logo_cnt,#background_container"
+        ).addClass("_hidden");
+    } else {
         $("#title_cnt,#logo_cnt").removeClass("_smalled");
-        $("#title_cnt,#tree_cnt,#logo_cnt,#background_container").removeClass("_hidden");
+        $("#title_cnt,#tree_cnt,#logo_cnt,#background_container").removeClass(
+            "_hidden"
+        );
         $("#timer_cnt,#presenter_cnt").addClass("_hidden");
     }
 });
--- main.js
+++ main.js
@@ -1,92 +1,101 @@
-require('dotenv').config()
+require("dotenv").config();
+
 const config = {
     WIDTH: parseInt(process.env.WIDTH) || 1024,
     HEIGHT: parseInt(process.env.HEIGHT) || 720,
     DEBUG: false,
-    MAIN_HOST: process.env.MAIN_HOST || "https://andresokol.herokuapp.com/",
+    MAIN_HOST: process.env.MAIN_HOST || "https://andresokol.herokuapp.com/"
 };
 
 if (process.env.DEBUG !== undefined) {
     config.DEBUG = true;
 }
 
-const electron = require('electron');
-const {app, BrowserWindow, dialog} = require('electron');
-const path = require('path');
-const url = require('url');
+const electron = require("electron");
+const { app, BrowserWindow, dialog } = require("electron");
+const path = require("path");
+const url = require("url");
 
-const log = require('electron-log');
+const log = require("electron-log");
 
-log.transports.file.level = 'silly';
-log.transports.console.level = 'silly';
+log.transports.file.level = "silly";
+log.transports.console.level = "silly";
 
 let win;
 
-function createWindow () {
+function createWindow() {
     global.SOCKET_HOST = config.MAIN_HOST;
 
-    log.info('SOCKET_HOST', global.SOCKET_HOST);
+    log.info("SOCKET_HOST", global.SOCKET_HOST);
 
     let displays = electron.screen.getAllDisplays();
 
-    message = 'Choose display to show overlay:';
+    message = "Choose display to show overlay:";
     options = [];
 
     for (let i = 0; i < displays.length; i += 1) {
-        options.push('Display ' + i + '. x=' + displays[i].bounds.x + 
-                     '; y=' + displays[i].bounds.y);
+        options.push(
+            "Display " +
+                i +
+                ". x=" +
+                displays[i].bounds.x +
+                "; y=" +
+                displays[i].bounds.y
+        );
     }
 
     log.info(message);
     log.info(options);
 
     let displayIndex = dialog.showMessageBox({
-        type: 'info',
-        message: 'Found ' + displays.length + ' displays.',
-        buttons: options,
+        type: "info",
+        message: "Found " + displays.length + " displays.",
+        buttons: options
     });
 
-    log.info('Chosen display:');
+    log.info("Chosen display:");
     log.info(displays[displayIndex]);
 
     let launch_display = displays[displayIndex].bounds;
 
     win = new BrowserWindow({
-                              x: launch_display.x,
-                              y: launch_display.y,
-                              width: parseInt(config.WIDTH),
-                              height: parseInt(config.HEIGHT),
-                              transparent: true,
-                              alwaysOnTop: !config.DEBUG, 
-                              frame: false,
-                              toolbar: false,
-                              kiosk: true,
+        x: launch_display.x,
+        y: launch_display.y,
+        width: parseInt(config.WIDTH),
+        height: parseInt(config.HEIGHT),
+        transparent: true,
+        alwaysOnTop: !config.DEBUG,
+        frame: false,
+        toolbar: false,
+        kiosk: true
     });
 
     win.setIgnoreMouseEvents(!config.DEBUG);
 
-    win.loadURL(url.format({
-        pathname: path.join(__dirname, 'index.html'),
-        protocol: 'file:',
-        slashes: true
-    }));
+    win.loadURL(
+        url.format({
+            pathname: path.join(__dirname, "index.html"),
+            protocol: "file:",
+            slashes: true
+        })
+    );
 
     //win.webContents.openDevTools();
 
-    win.on('closed', () => {
-        win = null
+    win.on("closed", () => {
+        win = null;
     });
 }
 
-app.on('ready', createWindow);
+app.on("ready", createWindow);
 
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit()
+app.on("window-all-closed", () => {
+    if (process.platform !== "darwin") {
+        app.quit();
     }
 });
 
-app.on('activate', () => {
+app.on("activate", () => {
     if (win === null) {
         createWindow();
     }
